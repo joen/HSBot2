ed5082e16739349b1ca933982a11a7883e5bd25a
hsbot <github@electronicfreak.de>
2016-04-06 19:26:43 +0200

    termine

---
+++ termine.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import config as c
+
+def abfall():
+	with open (c.cacPATH+"/"+i, "r") as myfile:
+		data = myfile.readlines()
+		n = len(data)
+		i = 0
+		while i<n:
+			
+		
