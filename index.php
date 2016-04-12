7641cc9dcf83340e656d04356780418e9a2acd5a
hsbot <github@electronicfreak.de>
2016-04-12 21:08:14 +0200

    web

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<html><head></head><body>
+<a href="index.php?a=infos">Infos</a><a href=""></a><hr/>
 <?php
-echo 'Hallo Welt';
+	if(isset($_GET['a'])) {
+		$a = $_GET['a'];
+	}else{
+		$a = '';
+	}
+	
+	if($a == 'infos') {
+		$d = opendir("info/");
+		while($f = readdir($d)) {
+			if(!is_dir("info/".$f)) {
+				echo $f .'<br/>';
+			}
+		}
+	}
 ?>
