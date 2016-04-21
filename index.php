7936a265bd80337b506b973278bf7548943d12fe
hsbot <github@electronicfreak.de>
2016-04-21 16:26:39 +0200

    www admin

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+	error_reporting(E_ALL); 
+	ini_set('error_reporting', E_ALL);
+?>
 <html><head></head><body>
 <a href="index.php?a=infos">Infos</a><a href=""></a><hr/>
 <?php
@@ -8,11 +12,52 @@
 	}
 	
 	if($a == 'infos') {
-		$d = opendir("info/");
-		while($f = readdir($d)) {
-			if(!is_dir("info/".$f)) {
-				echo $f .'<br/>';
+		if(isset($_GET['s'])) {
+			if($_GET['s'] == 'del') {
+				$f = str_replace('/','_',$_GET['f']);
+				unlink('info/'.$f.'.txt');
+				unset($_GET['f']);
 			}
+			
+			if($_GET['s'] == 'act') {
+				$f = str_replace('/','_',$_GET['f']);
+				if(substr($f,0,1) == '.') {
+					rename('info/'.$f.'.txt','info/'.substr($f,1) .'.txt');
+				}else{
+					rename('info/'.$f.'.txt','info/.'. $f .'.txt');
+				}
+				unset($_GET['f']);
+			}
+		}
+		
+		if(isset($_GET['f'])) {
+			$f = str_replace('/','_',$_GET['f']);
+			if(isset($_POST['submit'])) {
+				$h = fopen('info/'.$f.'.txt','w');
+				$content = str_replace('\r','',$_POST['content']);
+				flock($h,2);
+				fputs($h, $content);
+				flock($h,3);
+				fclose($h);
+			}else{
+				$content = file('info/'.$f.'.txt');
+				echo '<h2>'.$f.'</h2><form action="index.php?a=infos&f='. $f .'" method="post">
+					<textarea name="content" cols="30" rows="10">'. implode('',$content) .'</textarea><br/>
+					<input type="submit" name="submit" value="Editieren" />
+				</form>';
+			}
+		}else{
+			$d = opendir("info/");
+			while($f = readdir($d)) {
+				if(!is_dir("info/".$f)) {
+					$f = substr($f,0,-4);
+					echo '<a href="index.php?a=infos&f='. $f .'">'.$f.'</a> [<a href="index.php?a=infos&f='. $f .'&s=del">L&ouml;schen</a> <a href="index.php?a=infos&f='. $f .'&s=act">(De)Aktivieren</a>]<br/>';
+				}
+			}
+			echo '<form action="index.php" method="get">
+				<input type="hidden" name="a" value="infos" />
+				<input type="text" name="f" /> <input type="submit" value="Anlegen" />
+			</form';
 		}
 	}
 ?>
